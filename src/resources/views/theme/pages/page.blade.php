be59d58cc0c0a9c72231239392d49683f43bece7
nhienphan <phanvannhien@gmail.com>
2019-04-12 10:45:28 +0700

    fix link

--- src/resources/views/theme/pages/page.blade.php
+++ src/resources/views/theme/pages/page.blade.php
@@ -13,7 +13,7 @@
             {!! $page->content !!}
         </div>
         <p class="text-center">
-            <a href="{{ route('apply.visa') }}" class="btn btn-primary text-uppercase">Apply for Visa Now</a>
+            <a href="{{ route('apply.visa.step1') }}" class="btn btn-primary text-uppercase">Apply for Visa Now</a>
         </p>
 
     </div>
