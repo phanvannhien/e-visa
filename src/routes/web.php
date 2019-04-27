4024c2af40ee2033885ce6a6058d799bf7fe4f2b
nhienphan <phanvannhien@gmail.com>
2019-04-27 10:16:24 +0700

    fix https

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,7 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 
